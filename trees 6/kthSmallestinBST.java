3e6d18b4b1b0e24aeb5738ed1507281d7d77dca8
thecodersway <125991240+thecodersway@users.noreply.github.com>
2024-04-05 02:52:43 +0530

    Add files via upload

---
+++ trees 6/kthSmallestinBST.java
@@ -0,0 +1,35 @@
+
+class kthSmallestinBST
+{  public class TreeNode {
+   int val;
+   TreeNode left;
+     TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+        }
+     }
+    static  int c=0;
+    static int n=0;
+    void helper(TreeNode root)
+   {
+    if(root.left!=null)
+    helper(root.left);
+    c--;
+    if(c==0)
+    {
+        n=root.val;
+        return;
+    }
+    if(root.right!=null)
+    helper(root.right);
+   }
+    public int kthSmallest(TreeNode root, int k) {
+        c=k;
+       helper(root);
+       return n;
+    }
+}
