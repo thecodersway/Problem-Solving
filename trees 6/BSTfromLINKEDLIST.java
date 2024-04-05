a922c30a137380d49419b300d0d7409655dbcc45
thecodersway <125991240+thecodersway@users.noreply.github.com>
2024-04-06 00:17:49 +0530

    Add files via upload

---
+++ trees 6/BSTfromLINKEDLIST.java
@@ -0,0 +1,59 @@
+
+public class BSTfromLINKEDLIST {
+    //https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        if (prev != null) {
+            prev.next = null;
+            root.left = sortedListToBST(head);
+        }
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
