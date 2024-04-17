242eec8d14c2ca2f89a845761aa8c9ead3a83ca4
thecodersway <125991240+thecodersway@users.noreply.github.com>
2024-04-17 18:39:54 +0530

    Add files via upload

---
+++ Graph 1/DetectCycle.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class DetectCycle {
+    static boolean dfs(int s,ArrayList<ArrayList<Integer>> adj,boolean visited[],boolean path[])
+      {
+          visited[s]=true;
+          path[s]=true;
+          for(int i:adj.get(s))
+          {
+              if(!visited[i])
+              {
+                  if(dfs(i,adj,visited,path))
+                  return true;
+              }
+              else if(path[i])
+              return true;
+          }
+          path[s]=false;
+          return false;
+      }
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+      boolean visited[]=new boolean[V];
+      boolean path[]=new boolean[V];
+      for(int i=0;i<V;i++)
+      {
+          if(!visited[i])
+         { if(dfs(i,adj,visited,path))
+             return true;
+         }   
+      }  return false;
+    }
+}
+
